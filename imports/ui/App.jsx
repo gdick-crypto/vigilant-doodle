b0e9ca5fd1defff39fae56cf91931100dd4a658a
Giovanni Dick <giova19@pm.me>
2022-03-10 20:12:40 +0000

    Render Sample Tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from './Task.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>Simple Todo App</h1>
+    <ul>
+      { tasks.map(task => <Task key={task._id} task={task}/>)}
+    </ul>
   </div>
 );
 
